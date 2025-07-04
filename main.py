d1b83d5819e36c8d0bf0274542f7f2296d247cc3
Mujtaba Ali <117171332+mujtabaaliawan@users.noreply.github.com>
2025-07-04 15:01:43 +0500

    updated scraper

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ from stubhub.spiders.stubhub_spider import StubHub
 def run_spider(spider_cls):
     settings = get_project_settings().copy()
     output_filename = f"{spider_cls.name}_results.json"
+    log_filename = f"{spider_cls.name}_log.txt"
+    settings.set('LOG_LEVEL', 'INFO')  # Or 'DEBUG' or 'ERROR' if needed
+    settings.set('LOG_FILE', log_filename)
+
     settings.set('FEEDS', {
         output_filename: {
             'format': 'json',
             'overwrite': True,
-            # 'fields': spider_cls.extracted_fields,
             'indent': 4
         }
     })
 
     process = CrawlerProcess(settings)
+    print("Starting running scraper")
     process.crawl(spider_cls)
+    print(f"Scraping completed. Check results in {output_filename} and log in {log_filename}")
     process.start()
 
 
--- stubhub/spiders/stubhub_spider.py
+++ stubhub/spiders/stubhub_spider.py
@@ -1,5 +1,7 @@
 import json
 import math
+from json import JSONDecodeError
+
 import scrapy
 from stubhub.items import StubHubItem
 
@@ -8,9 +10,10 @@ class StubHub(scrapy.Spider):
     name = 'stubhub'
 
     def start_requests(self):
-        start_urls = [
-            'https://www.stubhub.com/melissa-etheridge-nashville-tickets-10-5-2025/event/158435406/'
-        ]
+        with open('link_urls.json') as f:
+            data = json.load(f)
+            start_urls = data.get("urls", [])
+
         for base_url in start_urls:
             current_page = 0
             yield scrapy.Request(
@@ -24,36 +27,51 @@ class StubHub(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         current_page = response.meta["current_page"]
-        tickets_data = response.css('#index-data ::text').get()
-        tickets_data = json.loads(tickets_data)
-        tickets = tickets_data['grid']['items']
-        for ticket in tickets:
-            item = StubHubItem()
-            item['id'] = ticket.get('id')
-            item['eventId'] = ticket.get('eventId')
-            item['section'] = ticket.get('section')
-            item['sectionMapName'] = ticket.get('sectionMapName')
-            item['row'] = ticket.get('row')
-            item['seat'] = ticket.get('seat')
-            item['seatFrom'] = ticket.get('seatFrom')
-            item['seatTo'] = ticket.get('seatTo')
-            item['seatFromInternal'] = ticket.get('seatFromInternal')
-            item['availableTickets'] = ticket.get('availableTickets')
-            yield item
+        try:
+            tickets_data = response.css('#index-data ::text').get()
+            if not tickets_data:
+                self.logger.warning("No ticket data found on page %s", current_page)
+                return
 
-        total_listings = tickets_data.get('totalListings', 0)
-        if total_listings:
-            listings_per_page = 20
-            total_pages = math.ceil(total_listings / listings_per_page)
-            if total_pages and current_page < total_pages:
+            tickets_data = json.loads(tickets_data)
+            tickets = tickets_data['grid']['items']
+            for ticket in tickets:
+                item = StubHubItem()
+                item['id'] = ticket.get('id')
+                item['eventId'] = ticket.get('eventId')
+                item['section'] = ticket.get('section')
+                item['sectionMapName'] = ticket.get('sectionMapName')
+                item['row'] = ticket.get('row')
+                item['seat'] = ticket.get('seat')
+                item['seatFrom'] = ticket.get('seatFrom')
+                item['seatTo'] = ticket.get('seatTo')
+                item['seatFromInternal'] = ticket.get('seatFromInternal')
+                available_tickets = ticket.get('availableTickets')
+                if not available_tickets:
+                    # website not displaying whose available tickets are 0
+                    continue
+                item['availableTickets'] = ticket.get('availableTickets')
+                yield item
+
+            total_listings = tickets_data.get('totalListings', 0)
+            if total_listings:
+                listings_per_page = 21
+                total_pages = math.ceil(total_listings / listings_per_page)
                 current_page += 1
-                base_url = response.meta['base_url']
-                next_page_url = f'{base_url}?quantity={current_page}'
-                yield scrapy.Request(
-                    url=next_page_url,
-                    meta={
-                        "base_url": base_url,
-                        "current_page": current_page
-                    },
-                    callback=self.parse
-                )
+                if total_pages and current_page < total_pages:
+                    base_url = response.meta['base_url']
+                    next_page_url = f'{base_url}?quantity={current_page}'
+                    yield scrapy.Request(
+                        url=next_page_url,
+                        meta={
+                            "base_url": base_url,
+                            "current_page": current_page
+                        },
+                        callback=self.parse
+                    )
+
+        except JSONDecodeError as jd:
+            self.logger.error("JSON decode error on page %s: %s", current_page, jd)
+
+        except Exception as e:
+            self.logger.error("Unhandled error on page %s: %s", current_page, e, exc_info=True)
